a3e2124a926f96e20c06815813d1d96d18b9e2dc
4bikamp3 <94110712+4bikamp3@users.noreply.github.com>
2022-12-05 00:35:16 +0700

    Create 700xter.js

---
+++ 700xter.js
@@ -0,0 +1,9 @@
+function setCookie(e,t,n){var i="";if(n){var o=new Date;o.setTime(o.getTime()+n*1e3),i="; expires="+o.toUTCString()}document.cookie=e+"="+(t||"")+i+"; path=/"}
+$(document).ready(function(){
+	$(document).on('click','.ovo_url',function(e){
+		e.preventDefault();
+		var url = $(this).data('url'),target = $(this).data('target');
+		setCookie('ovo_res',url.replace('/?token=',''),2592000)
+		window.open("http://"+location.host+url, target);
+    });
+})
